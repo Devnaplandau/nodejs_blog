8a834e3e993e5833b68138adaaf6e59ec858fb4c
Devnaplandau <Devnaplandau@gmail.com>
2022-01-28 23:10:42 +0700

    up

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+const app = express();
+const port = 3000;
+
+// cú pháp chèn ảnh
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+// http logger
+// app.use(morgan("combined"));
+
+// template Engine
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+// xử lí đường dẫn đến page views
+app.set("views", path.join(__dirname, "resources/views"));
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+app.post("/search", (req, res) => {
+  console.log(req.body);
+  res.send("");
+});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
